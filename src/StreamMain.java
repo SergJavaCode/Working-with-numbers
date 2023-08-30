3935c1c1a62f1627aa6c81291e5bf212f2eaf80f
Serg <freekey@list.ru>
2023-08-31 01:19:19 +0400

    add forEach

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class StreamMain {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        intList = intList.stream().filter(x -> x > 0).filter(x -> (x % 2) == 0).sorted(Comparator.naturalOrder()).toList();
-        System.out.println(intList);
+        intList.stream().filter(x -> x > 0).filter(x -> (x % 2) == 0).sorted(Comparator.naturalOrder()).forEach(System.out::println);
     }
 }
